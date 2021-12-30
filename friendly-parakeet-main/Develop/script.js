6c04ae950c00d3f907dc6deffb84d7ed40839373
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2021-12-30 11:29:37 -0600

    add arrays

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -3,9 +3,13 @@
 var promptLength
 var numbersTrue
 var symbolsTrue
+var uppercaseTrue
+var lowercaseTrue
 
-var characters = ['a', 'b']
+var charactersLower = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var characterUpper = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
+//function to determine if input is within length parameters
 function generatePassword() {
 promptLength = parseInt(window.prompt("How many characters would you like your password? (must be between 8 and 128)"));
 
@@ -14,18 +18,24 @@ promptLength = parseInt(window.prompt("How many characters would you like your p
     promptLength = window.prompt("Incorrect length input.  Please select a number between 8 and 128");
   
   }
-
-  if(promptLength > 8 || promptLength< 128) {
-    numbersTrue = confirm("will your password contain numbers?");
-    symbolsTrue = confirm("will your password contain symbols?")
-  }
-
-  
-};
-//function to determine if input is within length parameters
-
 // prompts to choose character types (lowercase, uppercase, numeric, and/or special characters)
 //atleast one character type should be selected
+  if(promptLength > 8 || promptLength< 128) {
+    numbersTrue = confirm("Will your password contain numbers?");
+    symbolsTrue = confirm("Will your password contain symbols?");
+    lowercaseTrue = confirm("Will your password contain lowercase letters?");
+    uppercaseTrue = confirm("Will your password contain uppercase letters?");
+  
+  while (!numbersTrue && !symbolsTrue && !lowercaseTrue && !uppercaseTrue){
+    window.alert ("You must select atleast one character type");
+    numbersTrue = confirm("Will your password contain numbers?");
+    symbolsTrue = confirm("Will your password contain symbols?");
+    lowercaseTrue = confirm("Will your password contain lowercase letters?");
+    uppercaseTrue = confirm("Will your password contain uppercase letters?");
+  }
+  }
+};
+
 
 //Password generate after all promts are answered
 
