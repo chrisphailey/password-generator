158a0e0c550e6a338fdfc79519bea3ef256b77b0
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2021-12-30 08:17:22 -0600

    length prompt

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
+// Prompt to choose password length between 8-128 characters
+var promptLength = window.prompt("How many characters would you like your password? (must be between 8 and 128)");
+  console.log("Password length of " + promptLength + " has been selected");
 
+
+//function to determine if input is within length parameters
+var passwordLength = function(){
+  if (promptLength < 8 || promptLength > 128) {
+    window.alert("Incorrect length input.  Please select a number between 8 and 128");
+  }
+}
+// prompts to choose character types (lowercase, uppercase, numeric, and/or special characters)
+//atleast one character type should be selected
+
+//Password generate after all promts are answered
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +29,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+passwordLength();
